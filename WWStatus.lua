25bc361dc42ec93d4f75200748a0c6d7a9fcadaa
Imitablemeat <89328914+Imitablemeat@users.noreply.github.com>
2024-06-07 06:45:57 -0500

    Update WWStatus.lua

--- WWStatus.lua
+++ WWStatus.lua
@@ -4,7 +4,7 @@ modem.transmit(15, os.computerID(), "Waterwheel")
 
 local event, side, channel, replyChannel, message, distance
 repeat
-  event, side, channel, replyChannel, message, distance
+  event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
 until channel == os.computerID()
 
 print("Receiving...\n")
