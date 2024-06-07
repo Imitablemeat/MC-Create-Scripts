2d28a529241496ebc452a87f40827fd4322759be
Imitablemeat <89328914+Imitablemeat@users.noreply.github.com>
2024-06-07 08:37:53 -0500

    Update RangeTest.lua

--- RangeTest.lua
+++ RangeTest.lua
@@ -4,12 +4,27 @@ local mo = peripheral.find("modem")
 mo.open(100)
  
 local loop = 5
-while loop != 0 do
+local timeout = 5
+
+while loop ~= 0 do
     sleep(2)
     mo.transmit(100, 100, "ping")
     print("Ping" .. textutils.formatTime(os.time()))
-    local event, channel, replyChannel, message, distance = os.pullEvent("modem_message")
-    print("Pong" .. distance)
-    loop -= 1
+    local eventData = {os.pullEvent()}
+    local event = eventData[1]
+	timeout = 5
+	
+	while timeout ~= 0 do
+    	if event == "modem_message"
+			print(toString(event[5]))
+			loop = loop - 1
+			
+		else
+			sleep(1)
+			timout = timeout -1
+			if timeout == 0
+				print("No Response" .. textutils.formatTime(os.time()))
+	end
     
 end
+
