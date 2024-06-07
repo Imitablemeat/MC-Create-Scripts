12c5a7dd3e297eb5dd065267ad9b5105de4f2f63
Imitablemeat <89328914+Imitablemeat@users.noreply.github.com>
2024-06-07 09:36:41 -0500

    Update RangeClient.lua

--- RangeClient.lua
+++ RangeClient.lua
@@ -8,8 +8,8 @@ while true do
 	local eventData = {os.pullEvent()}
 	local event = eventData[1]
 	if event == "modem_message" then
-		mo.transmit(replyChannel, 100, toString("Pong" .. math.floor(eventData[6])))
-		print("Pong" .. math.floor(eventData[6]))
+		mo.transmit(eventData[4], 100, tostring("Pong " .. math.floor(eventData[6])))
+		print("Pong " .. math.floor(eventData[6]))
 	end
 end
 
