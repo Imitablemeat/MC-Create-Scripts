0d9dcc21db5dcee9254ce4a02077771756e43954
Imitablemeat <89328914+Imitablemeat@users.noreply.github.com>
2024-06-07 09:14:42 -0500

    Update RangeTest.lua

--- RangeTest.lua
+++ RangeTest.lua
@@ -10,20 +10,21 @@ while loop ~= 0 do
     sleep(2)
     mo.transmit(100, 100, "ping")
     print("Ping" .. textutils.formatTime(os.time()))
-	loop = loop - 1
 	timeout = 5
 	
-	while timeout ~= 0 do
-	local eventData = {os.pullEvent()}
-    local event = eventData[1]
+	if timeout ~= 0 then
+		local eventData = {os.pullEvent()}
+		local event = eventData[1]
     	if event == "modem_message" then
 			print(tostring(eventData[5]))
+			loop = loop - 1
 			
 		else
 			sleep(1)
 			timout = timeout -1
 		if timeout == 0 then
 			print("No Response" .. textutils.formatTime(os.time()))
+			loop = loop - 1
 			end
 			
 		end
