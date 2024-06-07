ef511e62ed6abc4c6c381d88ab337bf2abca848b
Imitablemeat <89328914+Imitablemeat@users.noreply.github.com>
2024-06-07 09:47:40 -0500

    Update RangeTest.lua

--- RangeTest.lua
+++ RangeTest.lua
@@ -1,7 +1,6 @@
 local mo = peripheral.find("modem")
  
- 
-mo.open(100)
+ mo.open(100)
  
 local loop = 5
 local timeout = 5
@@ -9,7 +8,7 @@ local timeout = 5
 while loop ~= 0 do
     sleep(2)
     mo.transmit(100, 100, "ping")
-    print("Ping" .. textutils.formatTime(os.time()))
+    print("Ping " .. textutils.formatTime(os.time()))
 	timeout = 5
 	
 	if timeout ~= 0 then
@@ -22,10 +21,7 @@ while loop ~= 0 do
 		else
 			sleep(1)
 			timout = timeout -1
-		if timeout == 0 then
 			print("No Response" .. textutils.formatTime(os.time()))
-			loop = loop - 1
-			end
 			
 		end
 	end
