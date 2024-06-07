765fbc5065cac8d22b4e28269cc3f37a02e199c7
Imitablemeat <89328914+Imitablemeat@users.noreply.github.com>
2024-06-07 08:14:10 -0500

    Update RangeClient.lua

--- RangeClient.lua
+++ RangeClient.lua
@@ -3,8 +3,9 @@ local mo = peripheral.find("modem")
  
 mo.open(100)
 
-local event, channel, replyChannel, message, distance = os.pullEvent("modem_message")
-mo.transmit(replyChannel, 100, tostring(distance))
+local event, side, channel, replyChannel, distance, message = os.pullEvent("modem_message")
+local 
+mo.transmit(replyChannel, 100, toString("Pong" .. math.floor(distance)))
 print("Pong" .. distance)
 
     
